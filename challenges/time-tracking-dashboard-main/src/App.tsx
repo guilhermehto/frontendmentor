ec22eb8d1c0ad0dde8025efe1546e09a45751fd1
Guilherme Oliveira <htoguilherme@gmail.com>
2022-02-17 23:37:34 +1300

    feat: main mobile section

--- challenges/time-tracking-dashboard-main/src/App.tsx
+++ challenges/time-tracking-dashboard-main/src/App.tsx
@@ -1,7 +1,54 @@
 import React from "react";
 
+import { Card } from "./components/Card/Card";
+import { Profile } from "./components/Profile/Profile";
+import "./App.css";
+
 function App() {
-  return <div>Challenge goes here</div>;
+  return (
+    <div className="app-root">
+      <div className="cards-container">
+        <Profile />
+
+        <Card
+          title="Work"
+          information="32hrs"
+          subtitle="Last Week - 36hrs"
+          variation="work"
+        />
+        <Card
+          title="Work"
+          information="32hrs"
+          subtitle="Last Week - 36hrs"
+          variation="play"
+        />
+        <Card
+          title="Work"
+          information="32hrs"
+          subtitle="Last Week - 36hrs"
+          variation="study"
+        />
+        <Card
+          title="Work"
+          information="32hrs"
+          subtitle="Last Week - 36hrs"
+          variation="exercise"
+        />
+        <Card
+          title="Work"
+          information="32hrs"
+          subtitle="Last Week - 36hrs"
+          variation="social"
+        />
+        <Card
+          title="Work"
+          information="32hrs"
+          subtitle="Last Week - 36hrs"
+          variation="self-care"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
