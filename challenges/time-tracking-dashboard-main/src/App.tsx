ebfc0502f333b231a028d6260513c0c8d1c1eab3
Guilherme Oliveira <htoguilherme@gmail.com>
2022-02-18 23:52:38 +1300

    feat: desktop view structure

--- challenges/time-tracking-dashboard-main/src/App.tsx
+++ challenges/time-tracking-dashboard-main/src/App.tsx
@@ -7,49 +7,52 @@ import "./App.css";
 function App() {
   return (
     <div className="app-root">
-      <div className="cards-container">
-        <Profile
-          name="Jeremy Robson"
-          avatarPath="../../images/image-jeremy.png"
-          tabs={["Daily", "Weekly", "Monthly"]}
-        />
-
-        <Card
-          title="Work"
-          information="32hrs"
-          subtitle="Last Week - 36hrs"
-          variation="work"
-        />
-        <Card
-          title="Work"
-          information="32hrs"
-          subtitle="Last Week - 36hrs"
-          variation="play"
-        />
-        <Card
-          title="Work"
-          information="32hrs"
-          subtitle="Last Week - 36hrs"
-          variation="study"
-        />
-        <Card
-          title="Work"
-          information="32hrs"
-          subtitle="Last Week - 36hrs"
-          variation="exercise"
-        />
-        <Card
-          title="Work"
-          information="32hrs"
-          subtitle="Last Week - 36hrs"
-          variation="social"
-        />
-        <Card
-          title="Work"
-          information="32hrs"
-          subtitle="Last Week - 36hrs"
-          variation="self-care"
-        />
+      <div className="main-section">
+        <div className="profile-container">
+          <Profile
+            name="Jeremy Robson"
+            avatarPath="../../images/image-jeremy.png"
+            tabs={["Daily", "Weekly", "Monthly"]}
+          />
+        </div>
+        <div className="cards-container">
+          <Card
+            title="Work"
+            information="32hrs"
+            subtitle="Last Week - 36hrs"
+            variation="work"
+          />
+          <Card
+            title="Work"
+            information="32hrs"
+            subtitle="Last Week - 36hrs"
+            variation="play"
+          />
+          <Card
+            title="Work"
+            information="32hrs"
+            subtitle="Last Week - 36hrs"
+            variation="study"
+          />
+          <Card
+            title="Work"
+            information="32hrs"
+            subtitle="Last Week - 36hrs"
+            variation="exercise"
+          />
+          <Card
+            title="Work"
+            information="32hrs"
+            subtitle="Last Week - 36hrs"
+            variation="social"
+          />
+          <Card
+            title="Work"
+            information="32hrs"
+            subtitle="Last Week - 36hrs"
+            variation="self-care"
+          />
+        </div>
       </div>
     </div>
   );
