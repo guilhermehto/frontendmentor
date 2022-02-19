dc400128cb611cf5fc885c561fb1083a73da6c44
Guilherme Oliveira <htoguilherme@gmail.com>
2022-02-19 23:53:40 +1300

    feat: mock handler for dashboard data

--- challenges/time-tracking-dashboard-main/src/mocks/handlers.ts
+++ challenges/time-tracking-dashboard-main/src/mocks/handlers.ts
@@ -1,12 +1,25 @@
 import { rest } from "msw";
+import timeTrackingDashboardData from "../../data.json";
 
 export const handlers = [
   rest.get("/summary", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        answer: "yes",
-      })
-    );
+    const timeframe: string | null = req.url.searchParams.get("timeframe");
+
+    if (
+      timeframe !== "daily" &&
+      timeframe !== "weekly" &&
+      timeframe !== "monthly"
+    ) {
+      return res(ctx.status(400));
+    }
+
+    const data = timeTrackingDashboardData.map((item) => {
+      return {
+        title: item.title,
+        current: item.timeframes[timeframe].current,
+        previous: item.timeframes[timeframe].previous,
+      };
+    });
+    return res(ctx.status(200), ctx.json([...data]));
   }),
 ];
