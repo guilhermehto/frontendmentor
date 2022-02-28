0c950dd14c4a5e57d4ca14510a0cd8962317d5f5
Guilherme Oliveira <htoguilherme@gmail.com>
2022-02-28 18:00:16 +1300

    feat: add order summary texts

--- challenges/order-summary/src/components/Order/Order.tsx
+++ challenges/order-summary/src/components/Order/Order.tsx
@@ -2,7 +2,15 @@ import headerImage from '../../assets/illustration-hero.svg';
 import './Order.css';
 
 const Order = () => {
-  return <div className="order"><HeaderImage /></div>
+  return <div className="order">
+    <HeaderImage />
+    <div className="order-summary">
+      <h2>Order Summary</h2>
+      <p>
+        You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!
+      </p>
+  </div>
+  </div>
 }
 
 const HeaderImage = () => {
