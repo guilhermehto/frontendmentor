e4f91af6525a46016bdf2897556162e834052242
Guilherme Oliveira <htoguilherme@gmail.com>
2022-03-10 22:29:07 +1300

    feat: apply theme colors to global styles

--- challenges/github-user-search/src/styles/GlobalStyles.tsx
+++ challenges/github-user-search/src/styles/GlobalStyles.tsx
@@ -12,28 +12,34 @@ const GlobalStyles = createGlobalStyle`
     font-family: "Space Mono";
     font-size: 16px;
     line-height: 25px;
+    background-color: ${(props) => props.theme.backgroundColor};
+    color: ${(props) => props.theme.textColor}
   }
 
   h1 {
     font-weight: 700;
     font-size: 26px;
     line-height: 38px;
+    color: ${(props) => props.theme.headingColor}
   }
 
   h2 {
     font-weight: 700;
     font-size: 22px;
     line-height: 33px;
+    color: ${(props) => props.theme.headingColor}
   }
 
   h3 {
     font-size: 16px;
     line-height: 24px;
+    color: ${(props) => props.theme.headingColor}
   }
 
   h4 {
     font-size: 13px;
     line-height: 20px;
+    color: ${(props) => props.theme.headingColor}
   }
 `
 
