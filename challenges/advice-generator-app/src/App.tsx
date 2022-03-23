d0f37d2710ddf42bb564fe54e89b25a2cbf043c7
Guilherme Oliveira <htoguilherme@gmail.com>
2022-03-23 23:32:23 +1300

    feat: query advices from api

--- challenges/advice-generator-app/src/App.tsx
+++ challenges/advice-generator-app/src/App.tsx
@@ -1,20 +1,27 @@
+import { useState } from 'react'
+
+import { advicesClient } from './clients/advices-client'
 import { Advice } from './components/Advice/Advice'
 import { AdviceButton } from './components/Advice/AdviceButton'
 import { AdviceTitle } from './components/Advice/AdviceTitle'
 
 function App() {
-	const onButtonClicked = () => {
-		console.log('button clicked')
+	const [slip, setSlip] = useState<{ id: number; advice: string }>({
+		id: 117,
+		advice: "It is easy to sit up and take notice, what's difficult is getting up and taking action.",
+	})
+	const onButtonClicked = async () => {
+		const newAdvice = await advicesClient.getRandomAdvice()
+		setSlip(newAdvice)
 	}
 
 	return (
 		<main className="font-body bg-dark-blue h-screen flex items-center justify-center px-4">
 			<Advice
-				title={<AdviceTitle>Advice #117</AdviceTitle>}
+				title={<AdviceTitle>{`Advice #${slip.id}`}</AdviceTitle>}
 				button={<AdviceButton onClick={onButtonClicked} />}
 			>
-				"It is easy to sit up and take notice, what's difficult is getting up and taking
-				action."
+				"{slip.advice}"
 			</Advice>
 		</main>
 	)
