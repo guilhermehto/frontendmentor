a369741bd95bec012b77dba604a07cdfa0f0cdcf
Guilherme Oliveira <htoguilherme@gmail.com>
2022-03-23 18:14:06 +1300

    feat: style advice text

--- challenges/advice-generator-app/src/App.tsx
+++ challenges/advice-generator-app/src/App.tsx
@@ -5,10 +5,8 @@ function App() {
 	return (
 		<main className="font-body bg-dark-blue h-screen flex items-center justify-center px-4">
 			<Advice title={<AdviceTitle>Advice #117</AdviceTitle>}>
-				<h1 className="text-cyan">
-					"It is easy to sit up and take notice, what's difficult is getting up and taking
-					action."
-				</h1>
+				"It is easy to sit up and take notice, what's difficult is getting up and taking
+				action."
 			</Advice>
 		</main>
 	)
--- challenges/advice-generator-app/src/components/Advice/Advice.tsx
+++ challenges/advice-generator-app/src/components/Advice/Advice.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import mobileDividerSvg from '../../assets/pattern-divider-mobile.svg'
 
 type AdviceProps = {
 	title: ReactNode
@@ -6,9 +7,10 @@ type AdviceProps = {
 
 const Advice: React.FC<AdviceProps> = ({ children, title }) => {
 	return (
-		<div className="bg-dark-gray-blue rounded-xl px-6 py-10 text-center">
+		<div className="bg-dark-gray-blue rounded-xl px-6 py-10 text-center flex flex-col gap-6">
 			{title}
-			{children}
+			<h1 className="text-cyan text-2xl">{children}</h1>
+			<img src={mobileDividerSvg} />
 		</div>
 	)
 }
