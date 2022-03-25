7249dd79a0c5dd8fb48aa13ab807ba04941083cc
Guilherme Oliveira <htoguilherme@gmail.com>
2022-03-26 12:41:58 +1300

    feat: theme colors

--- challenges/job-listings/tailwind.config.js
+++ challenges/job-listings/tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		colors: {
+			'desaturated-cyan': 'hsl(180, 29%, 50%)',
+			cyan: 'hsl(180, 52%, 96%)',
+			'light-cyan': 'hsl(180, 31%, 95%)',
+			'dark-cyan': 'hsl(180, 8%, 52%)',
+			'very-dark-cyan': 'hsl(180, 14%, 20%)',
+		},
 	},
 	plugins: [],
 }
