e3e94cd9924f9d26f80240644a0300e488e58247
Guilherme Oliveira <htoguilherme@gmail.com>
2022-03-26 12:42:14 +1300

    feat: add header

--- challenges/job-listings/src/App.tsx
+++ challenges/job-listings/src/App.tsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header'
+
 function App() {
 	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
-		</div>
+		<main>
+			<Header />
+			<h1 className="text-3xl font-bold">Job listings</h1>
+		</main>
 	)
 }
 
---
+++ challenges/job-listings/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import mobileHeader from '../assets/bg-header-mobile.svg'
+
+const Header = () => {
+	return (
+		<header className="bg-desaturated-cyan">
+			<img src={mobileHeader} alt="green shapes" />
+		</header>
+	)
+}
+
+export { Header }
