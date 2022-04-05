081b7dd5f0cc4ff3d03730e2934b511877b188e5
Guilherme Oliveira <htoguilherme@gmail.com>
2022-04-05 23:23:13 +1200

    chore: initial theme colors

---
+++ challenges/memory-game/src/theme/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+	orange: 'hsl(37, 98%, 54%)',
+	blue: 'hsl(205, 37%, 55%)',
+	white: 'hsl(0, 0%, 99%)',
+	'blue-gray': 'hsl(293, 28%, 79%)',
+	'dark-blue-gray': 'hsl(205, 30%, 27%)',
+	'dark-gray': 'hsl(206, 45%, 15%)',
+	'light-gray': 'hsl(0,0%, 95%)',
+	'light-blue': 'hsl(203, 22%, 55%)',
+}
+
+export { colors }
