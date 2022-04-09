3d3b4f270b3b0a63ff5e36764028901b364b4253
Guilherme Oliveira <htoguilherme@gmail.com>
2022-04-09 22:46:23 +1200

    chore: update heading story

--- challenges/memory-game/src/components/Heading/Heading.stories.tsx
+++ challenges/memory-game/src/components/Heading/Heading.stories.tsx
@@ -1,9 +1,23 @@
+import type { Story } from '@ladle/react'
+
 import { GlobalStyles } from '../../theme/GlobalStyles'
 import { Heading as HeadingComponent } from './Heading'
 
-export const Heading = () => (
+export default {
+	title: 'Typography',
+}
+
+export const Heading: Story<{
+	level: 1 | 2 | 3
+	text: string
+}> = ({ level = 2, text }) => (
 	<>
 		<GlobalStyles />
-		<HeadingComponent level={1}>Heading story</HeadingComponent>
+		<HeadingComponent level={level}>{text}</HeadingComponent>
 	</>
 )
+
+Heading.args = {
+	level: 2,
+	text: 'Heading Story',
+}
