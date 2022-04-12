0795579296269614f7955cafc9e3c6b5ef9994b8
Guilherme Oliveira <htoguilherme@gmail.com>
2022-04-12 22:24:20 +1200

    feat: default text color

--- challenges/memory-game/src/theme/GlobalStyles.tsx
+++ challenges/memory-game/src/theme/GlobalStyles.tsx
@@ -6,6 +6,7 @@ const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: 'Atkinson Hyperlegible', sans;
+    color: ${(props) => props.theme.textColor};
   }
   body {
       font-size: 18px;
