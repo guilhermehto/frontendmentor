ed562f4420ef5438563b5e902d5ee52911476807
Guilherme Oliveira <htoguilherme@gmail.com>
2022-09-03 13:29:35 +1200

    fix(Card): missing padding

--- challenges/interactive-rating-component/src/Components/Card.tsx
+++ challenges/interactive-rating-component/src/Components/Card.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+
 type CardProps = {
 	children: React.ReactNode
 }
 
 const Card: React.FC<CardProps> = ({ children }) => {
-	return <div className="rounded-lg bg-dark-gradient">{children}</div>
+	return <div className="rounded-lg bg-dark-gradient p-6">{children}</div>
 }
 
 export { Card }
