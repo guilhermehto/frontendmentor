a351e6cd6708d03b81a35b7df187799660ed24de
Guilherme Oliveira <htoguilherme@gmail.com>
2022-09-03 12:31:51 +1200

    feat: add interactive component challenge

---
+++ challenges/interactive-rating-component/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './src/**/*.{js, ts, tsx}'],
+	theme: {
+		fontFamily: {
+			display: ['Overpass'],
+			body: ['Overpass'],
+		},
+		extend: {},
+	},
+	plugins: [],
+}
