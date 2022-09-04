ffaa2d9814f831b4a2ff79163c195cacd73f1f4a
Guilherme Oliveira <htoguilherme@gmail.com>
2022-09-04 19:09:30 +1200

    fix: heading text should be bold

--- challenges/interactive-rating-component/src/Components/Heading.tsx
+++ challenges/interactive-rating-component/src/Components/Heading.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 type HeadingProps = {
 	children: string
-	className: string
+	className?: string
 }
 
 const Heading: React.FC<HeadingProps> = ({ children, className }) => {
-	return <h1 className={`text-2xl text-white ${className}`}>{children}</h1>
+	return <h1 className={`text-2xl text-white font-bold ${className}`}>{children}</h1>
 }
 
 export { Heading }
