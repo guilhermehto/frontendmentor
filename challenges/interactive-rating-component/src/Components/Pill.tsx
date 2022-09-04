85f6557cbb6b91448e2efdc77013620ecb93b27f
Guilherme Oliveira <htoguilherme@gmail.com>
2022-09-04 18:34:03 +1200

    feat(Pill): accept classname

--- challenges/interactive-rating-component/src/Components/Pill.tsx
+++ challenges/interactive-rating-component/src/Components/Pill.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 type PillProps = {
 	children: string
+	className?: string
 }
 
-const Pill: React.FC<PillProps> = ({ children }) => {
+const Pill: React.FC<PillProps> = ({ children, className }) => {
 	return (
-		<div className="bg-dark-blue text-orange text-center text-sm leading-none rounded-full px-3 py-1">
+		<div
+			className={`bg-dark-blue text-orange text-center text-sm leading-none rounded-full flex justify-center items-end py-2 ${className}`}
+		>
 			{children}
 		</div>
 	)
