628df1e1b91af80368a16f402411f7f31bd54332
Guilherme Oliveira <htoguilherme@gmail.com>
2022-11-08 01:49:43 -0300

    feat: heading component

---
+++ apps/expenses-chart-component/src/components/heading/heading.tsx
@@ -0,0 +1,41 @@
+import { component$, Slot, useStyles$ } from '@builder.io/qwik'
+
+import styles from './heading.css'
+
+type HeadingProps = {
+	level?: 1 | 2 | 3 | 4
+	color?: 'white' | 'brown'
+}
+
+export const Heading = component$(({ level = 1, color = 'brown' }: HeadingProps) => {
+	useStyles$(styles)
+
+	switch (level) {
+		case 2:
+			return (
+				<>
+					<h2 className={color}>
+						<Slot />
+					</h2>
+				</>
+			)
+		case 3:
+			return (
+				<h3 className={color}>
+					<Slot />
+				</h3>
+			)
+		case 4:
+			return (
+				<h4 className={color}>
+					<Slot />
+				</h4>
+			)
+		default:
+			return (
+				<h1 className={color}>
+					<Slot />
+				</h1>
+			)
+	}
+})
