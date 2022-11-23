9ba57c5c032e058d514a38cbeba9b11e45d6a0a0
Guilherme Oliveira <htoguilherme@gmail.com>
2022-11-23 13:08:21 -0300

    feat: regular sized caption

--- apps/expenses-chart-component/src/components/caption/caption.tsx
+++ apps/expenses-chart-component/src/components/caption/caption.tsx
@@ -4,12 +4,12 @@ import { clsx } from 'clsx'
 import styles from './caption.css'
 
 type CaptionProps = {
-	size?: 'mini'
+	size?: 'mini' | 'regular'
 	center?: boolean
 }
 
 export const Caption = component$(({ size = 'mini', center }: CaptionProps) => {
 	useStylesScoped$(styles)
-	const classes = clsx(size, center && 'center')
+	const classes = clsx(size, 'caption', center && 'center')
 	return <p class={classes}>{<Slot />}</p>
 })
