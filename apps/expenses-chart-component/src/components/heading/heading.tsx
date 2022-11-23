67f68bf07549ad486b5c0dacdce811a837daa9d0
Guilherme Oliveira <htoguilherme@gmail.com>
2022-11-23 12:53:19 -0300

    feat: center heading text

--- apps/expenses-chart-component/src/components/heading/heading.tsx
+++ apps/expenses-chart-component/src/components/heading/heading.tsx
@@ -1,39 +1,42 @@
 import { component$, Slot, useStylesScoped$ } from '@builder.io/qwik'
+import { clsx } from 'clsx'
 
 import styles from './heading.css'
 
 type HeadingProps = {
 	level?: 1 | 2 | 3 | 4
 	color?: 'white' | 'brown'
+	center?: boolean
 }
 
-export const Heading = component$(({ level = 1, color = 'brown' }: HeadingProps) => {
+export const Heading = component$(({ level = 1, color = 'brown', center }: HeadingProps) => {
 	useStylesScoped$(styles)
+	const classes = clsx(color, center && 'center')
 
 	switch (level) {
 		case 2:
 			return (
 				<>
-					<h2 className={color}>
+					<h2 className={classes}>
 						<Slot />
 					</h2>
 				</>
 			)
 		case 3:
 			return (
-				<h3 className={color}>
+				<h3 className={classes}>
 					<Slot />
 				</h3>
 			)
 		case 4:
 			return (
-				<h4 className={color}>
+				<h4 className={classes}>
 					<Slot />
 				</h4>
 			)
 		default:
 			return (
-				<h1 className={color}>
+				<h1 className={classes}>
 					<Slot />
 				</h1>
 			)
